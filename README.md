a8dcc2d87847cb0fd3425b65b44ec51ecdab0da0
DevonArtmeier <devon.artmeier@outlook.com>
2022-12-06 06:33:39 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Sega Genesis Scroll Demo
+A very basic demo for scrolling on the Sega Genesis. Draws from a 1024x1024 tilemap onto a 512x256px plane and repeats infinitely.
+
+## Controls
+* D-Pad: Move map around
---
+++ Variables.asm
@@ -0,0 +1,38 @@
+; -------------------------------------------------------------------------
+; Genesis Scrolling Demo
+; By Devon Artmeier 2022
+; -------------------------------------------------------------------------
+
+; -------------------------------------------------------------------------
+; Constants
+; -------------------------------------------------------------------------
+
+ROWTILECNT	EQU	(320/8)+4		; Number of tiles per row
+COLTILECNT	EQU	(224/8)+4		; Number of tiles per blocks
+
+; -------------------------------------------------------------------------
+; RAM
+; -------------------------------------------------------------------------
+
+	rsset	$FFFF8000
+frameCount	rs.w	1			; Frame count
+
+ctrlData	rs.b	0			; Controller data
+ctrlHold	rs.b	1			; Controller held buttons
+ctrlTap		rs.b	1			; Controller tapped buttons
+
+cameraX		rs.w	1			; Camera X position
+cameraY		rs.w	1			; Camera Y position
+
+prevCamX	rs.w	1			; Previous camera X position
+prevCamY	rs.w	1			; Previous camera Y position
+
+colVDPCmd	rs.l	1			; Column VDP command
+colSect1Cnt	rs.w	1			; Column section 1 block count
+rowVDPCmd	rs.l	1			; Row VDP command
+rowSect1Cnt	rs.w	1			; Row section 1 block count
+
+colBuffer	rs.w	COLTILECNT*2		; Column buffer
+rowBuffer	rs.w	ROWTILECNT*2		; Row buffer
+
+; -------------------------------------------------------------------------
